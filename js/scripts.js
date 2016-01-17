81419fd09c136683e479bc44f48818c75b57a854
Tyler Suderman <tylersuderman@tyler-sudermans-macbook.local>
2016-01-17 14:05:07 -0800

    tweak faq section again to kerry's prefferences

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,25 @@ $("#question1").click(function() {
   $("#faqdrop2").slideUp();
   $("#faqdrop3").slideUp();
   $("#faqdrop1").slideToggle();
+  $("#question1 h2").addClass("active");
+  $("#question2 h2").removeClass("active");
+  $("#question3 h2").removeClass("active");
 });
 
 $("#question2").click(function() {
   $("#faqdrop1").slideUp();
   $("#faqdrop3").slideUp();
   $("#faqdrop2").slideToggle();
+  $("#question2 h2").addClass("active");
+  $("#question1 h2").removeClass("active");
+  $("#question3 h2").removeClass("active");
 });
 
 $("#question3").click(function() {
   $("#faqdrop1").slideUp();
   $("#faqdrop2").slideUp();
   $("#faqdrop3").slideToggle();
+  $("#question3 h2").addClass("active");
+  $("#question1 h2").removeClass("active");
+  $("#question2 h2").removeClass("active");
 });
