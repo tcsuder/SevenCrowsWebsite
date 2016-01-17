1845b4101d93ae8593e2c71b42dc7dd5e887afc9
Tyler Suderman <tylersuderman@tyler-sudermans-macbook.local>
2016-01-17 14:07:51 -0800

    hot fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $("#question1").click(function() {
   $("#faqdrop2").slideUp();
   $("#faqdrop3").slideUp();
   $("#faqdrop1").slideToggle();
-  $("#question1 h2").addClass("active");
+  $("#question1 h2").toggleClass("active");
   $("#question2 h2").removeClass("active");
   $("#question3 h2").removeClass("active");
 });
@@ -11,7 +11,7 @@ $("#question2").click(function() {
   $("#faqdrop1").slideUp();
   $("#faqdrop3").slideUp();
   $("#faqdrop2").slideToggle();
-  $("#question2 h2").addClass("active");
+  $("#question2 h2").toggleClass("active");
   $("#question1 h2").removeClass("active");
   $("#question3 h2").removeClass("active");
 });
@@ -20,7 +20,7 @@ $("#question3").click(function() {
   $("#faqdrop1").slideUp();
   $("#faqdrop2").slideUp();
   $("#faqdrop3").slideToggle();
-  $("#question3 h2").addClass("active");
+  $("#question3 h2").toggleClass("active");
   $("#question1 h2").removeClass("active");
   $("#question2 h2").removeClass("active");
 });
