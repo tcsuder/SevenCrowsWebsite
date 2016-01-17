4e00ce9189c8acface35d9d1f167fd40f1ac1240
Tyler Suderman <tylersuderman@tyler-sudermans-macbook.local>
2016-01-17 12:53:30 -0800

    polish styling and fix minor punctuation inconsistencies

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $("#question1").click(function() {
+  $("#faqdrop2").slideUp();
+  $("#faqdrop3").slideUp();
   $("#faqdrop1").slideToggle();
 });
 
 $("#question2").click(function() {
+  $("#faqdrop1").slideUp();
+  $("#faqdrop3").slideUp();
   $("#faqdrop2").slideToggle();
 });
 
 $("#question3").click(function() {
+  $("#faqdrop1").slideUp();
+  $("#faqdrop2").slideUp();
   $("#faqdrop3").slideToggle();
 });
