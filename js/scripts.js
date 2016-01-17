cdddb9c4105c27970c92c7328c35cff2a8b635e0
Tyler Suderman <tylersuderman@tyler-sudermans-macbook.local>
2016-01-17 10:10:20 -0800

    add jQuery function to faq and fix editorial errors

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$("#question1").click(function() {
+  $("#faqdrop1").slideToggle();
+});
+
+$("#question2").click(function() {
+  $("#faqdrop2").slideToggle();
+});
+
+$("#question3").click(function() {
+  $("#faqdrop3").slideToggle();
+});
